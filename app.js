22e6d055105a2c1563b691f4c8568038fbf9d91b
Jack <60860781+jack-nonnie@users.noreply.github.com>
2020-05-18 21:17:30 -0400

    First commit

--- app.js
+++ app.js
@@ -1,19 +1,16 @@
-'use strict';
+"use strict";
 
-const director = require('director');
-const Server   = require('./lib/server');
+const director = require("director");
+const Server = require("./lib/server");
 
-// Create a router for GET and POST requests to the app
 const router = new director.http.Router({
-    '/': {
+    "/": {
         post: Server.postResponse,
-        get: Server.getResponse
-    }
+        get: Server.getResponse,
+    },
 });
 
-// Check if the `--dev` flag was passed
-const devMode = process.argv[2] === '--dev';
+const devMode = process.argv[2] === "--dev";
 
-// Start listening
 const server = new Server(router, devMode);
 server.serve();
